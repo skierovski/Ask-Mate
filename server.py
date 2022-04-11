0099361f558f8a78e3b68b4d46dcb5cb62eb9c49
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-04-11 22:54:25 +0200

    Continue working on details of question

--- server.py
+++ server.py
@@ -14,6 +14,13 @@ def questions():
     user_questions = data_handler.get_questions()
     return render_template('list.html', questions = user_questions)
 
+@app.route("/questions/<question_id>")
+def question_details(question_id):
+    questions = data_handler.get_questions()
+    for item in questions:
+        if item["id"] == question_id:
+            question = item
+    return render_template("deatails.html", selected_question = question)
 
 if __name__ == "__main__":
     app.run()
