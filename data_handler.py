a32ef5f09464812cd673e1af4488b596aa16cb1a
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-04-11 22:36:18 +0200

    Showing list of questions in /questions and starting to go to question details by link in title for every question ;D

---
+++ data_handler.py
@@ -0,0 +1,16 @@
+import csv
+import os
+QUESTIONS = "C:/Users/salon/Codecool/ask-mate-1-python-AdrianRymaszewski/sample_data/question.csv"
+ANSWERS = "C:/Users/salon/Codecool/ask-mate-1-python-AdrianRymaszewski/sample_data/answer.csv"
+QUESTIONS_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message"]
+
+def get_questions():
+    questions = {}
+    result = []
+    with open(QUESTIONS) as file:
+        for row in file:
+            record = row.split(",")
+            for i in range(len(record)):
+                questions[QUESTIONS_HEADERS[i]] = record[i]
+            result.append(questions)
+        return result
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import data_handler
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/questions")
+def questions():
+    user_questions = data_handler.get_questions()
+    return render_template('list.html', questions = user_questions)
 
 
 if __name__ == "__main__":
