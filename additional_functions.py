fa1fb324963cd0cdeb97e2a7c063a9765bbef710
AdrianRymaszewski <arymasz87@gmail.com>
2022-04-14 21:33:52 +0200

    After refactor

--- additional_functions.py
+++ additional_functions.py
@@ -1,10 +1,33 @@
 import datetime
+import os
 
 
-def create_name_file(string):
+def create_name_file(file_name):
     seconds_since_epoch = str(datetime.datetime.now().timestamp()).replace(".", '')
-    file_type = string[-3:]
-    string = string.replace("." + file_type, "")
-    img_name = ''.join(filter(str.isalnum, string))
-    img_name = img_name + str(seconds_since_epoch) + "." + file_type
-    return img_name
+    file_type = file_name[-3:]
+    file_name = file_name.replace("." + file_type, "")
+    img_name = ''.join(filter(str.isalnum, file_name))
+    return f'{img_name}{str(seconds_since_epoch)}.{file_type}'
+
+
+def get_index(list_to_check):
+    if len(list_to_check) == 0:
+        return 0
+    last_id = list_to_check[-1]['id']
+    new_id = str(int(last_id) + 1)
+    return new_id
+
+
+def file_operation(file_list):
+    if file_list.filename != "":
+        file_name = create_name_file(file_list.filename)
+        file_list.save(os.path.join('static', file_name))
+        return file_name
+    return "img.png"
+
+
+def vote(questions, id, direction):
+    for item in questions:
+        if item['id'] == id:
+            item['vote_number'] = str(int(item['vote_number']) + direction)
+    return questions
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import datetime
 from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import os
 import data_handler
 import additional_functions
 
@@ -29,6 +27,7 @@ templates_answer = {
     'image': "img.png"
 }
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -54,89 +53,68 @@ def view_question(q_id):
 def add_new_question():
     if request.method == "GET":
         return render_template('newquestion.html')
-    if request.method == "POST":
-        new_question = templates_question.copy()
-        question = data_handler.get_all_question()
-        if len(question)==0:
-            new_question['id'] = "0"
-        else:
-            last_id = question[-1]['id']
-            new_question['id'] = str(int(last_id) + 1)
-        new_question['submission_time'] = str(datetime.datetime.now().strftime("%d/%m/%y %H:%M"))
-        if 'title' in request.form:
-            new_question['title'] = request.form['title']
-        if 'message' in request.form:
-            new_question['message'] = request.form['message']
-        f = request.files['file']
-        if f.filename != "":
-            file_name = additional_functions.create_name_file(f.filename)
-            new_question['image'] = file_name
-            f.save(os.path.join('static', file_name))
-        question.append(new_question)
-        data_handler.write_table_to_file_question(data_handler.create_list_to_write(question))
-        link_direct = "/question/" + str(new_question['id'])
-    return redirect(link_direct)
+
+    new_question = templates_question.copy()
+    questions = data_handler.get_all_question()
+    new_question['id'] = additional_functions.get_index(questions)
+    new_question['submission_time'] = str(datetime.datetime.now().strftime("%d/%m/%y %H:%M"))
+    new_question['title'] = request.form['title'] if request.form['title'] else ""
+    new_question['message'] = request.form['message'] if request.form['message'] else ""
+    upload_file = request.files['file']
+    new_question['image'] = additional_functions.file_operation(upload_file)
+    questions.append(new_question)
+    data_handler.write_table_to_file_question(data_handler.create_list_to_write(questions))
+    return redirect(f"/question/{str(new_question['id'])}")
 
 
 @app.route('/question/<question_id>/new-answer', methods=['POST', 'GET'])
 def add_answer(question_id):
     if request.method == "GET":
         return render_template('newanswer.html', question_id=question_id)
-    if request.method == "POST":
-        answer = data_handler.get_all_answer()
-        new_answer = templates_answer.copy()
-        if len(answer) == 0:
-            new_answer['id'] = 0
-        else:
-            last_id = answer[-1]['id']
-            new_answer['id'] = str(int(last_id) + 1)
-        new_answer['submission_time'] = str(datetime.datetime.now().strftime("%d/%m/%y %H:%M"))
-        new_answer['question_id'] = question_id
-        if 'message' in request.form:
-            new_answer['message'] = request.form['message']
-        f = request.files['file_answer']
-        if f.filename != "":
-            file_name = additional_functions.create_name_file(f.filename)
-            new_answer['image'] = file_name
-            f.save(os.path.join('static', file_name))
-        answer.append(new_answer)
-        data_handler.write_table_to_file_answer(data_handler.create_list_to_write(answer))
-        link_direct = "/question/" + str(question_id)
-    return redirect(link_direct)
+
+    answer = data_handler.get_all_answer()
+    new_answer = templates_answer.copy()
+    new_answer['id'] = additional_functions.get_index(answer)
+    new_answer['submission_time'] = str(datetime.datetime.now().strftime("%d/%m/%y %H:%M"))
+    new_answer['question_id'] = question_id
+    new_answer['message'] = request.form['message'] if 'message' in request.form else ""
+    upload_file = request.files['file_answer']
+    new_answer['image'] = additional_functions.file_operation(upload_file)
+    answer.append(new_answer)
+    data_handler.write_table_to_file_answer(data_handler.create_list_to_write(answer))
+    return redirect(f"/question/{str(question_id)}")
 
 
 @app.route('/question/<q_id>/delete')
 def delete_question(q_id):
-    question = data_handler.get_all_question()
-    for item in question:
-        if item['id'] == str(q_id):
-            question.remove(item)
-    data_handler.write_table_to_file_question(data_handler.create_list_to_write(question))
+    questions = data_handler.get_all_question()
+    questions = [item for item in questions if item['id'] != str(q_id)]
+    data_handler.write_table_to_file_question(data_handler.create_list_to_write(questions))
     return redirect("/list")
 
-@app.route('/question/<q_id>/edit',methods=['POST', 'GET'])
+
+@app.route('/question/<q_id>/edit', methods=['POST', 'GET'])
 def edit_question(q_id):
-    if request.method=='GET':
+    if request.method == 'GET':
         select_question = data_handler.get_all_question(q_id)
         return render_template('editquestion.html', selected_question=select_question)
-    if request.method =='POST':
-        questions = data_handler.get_all_question()
-        if 'title' in request.form:
-            questions[int(q_id)]['title'] = request.form['title']
-        if 'message' in request.form:
-            questions[int(q_id)]['message'] = request.form['message']
-        data_handler.write_table_to_file_question(data_handler.create_list_to_write(questions))
-        link_direct = "/question/" + str(q_id)
-        return redirect(link_direct)
+
+    questions = data_handler.get_all_question()
+    for item in questions:
+        if item['id'] == str(q_id):
+            item['title'] = request.form['title'] if 'title' in request.form else item['title']
+            item['message'] = request.form['message'] if 'message' in request.form else item['message']
+    data_handler.write_table_to_file_question(data_handler.create_list_to_write(questions))
+    return redirect(f"/question/{str(q_id)}")
 
 
 @app.route('/answer/<answer_id>/delete')
 def delete_answer(answer_id):
     answers = data_handler.get_all_answer()
-    question_id=""
+    question_id = ""
     for item in answers:
         if item['id'] == str(answer_id):
-            question_id=item['question_id']
+            question_id = item['question_id']
             answers.remove(item)
     data_handler.write_table_to_file_answer(data_handler.create_list_to_write(answers))
     link_direct = "/question/" + str(question_id)
@@ -146,14 +124,15 @@ def delete_answer(answer_id):
 @app.route('/question/<question_id>/<vote>')
 def vote_question(question_id, vote):
     questions = data_handler.get_all_question()
-    if vote == 'vote-up':
-        for item in questions:
-            if item['id']==question_id:
-                item['vote_number']= str(int(item['vote_number'])+1)
-    elif vote == 'vote-down':
-        for item in questions:
-            if item['id'] == question_id:
-                item['vote_number'] = str(int(item['vote_number']) + 1)
+    questions = additional_functions.vote(questions, question_id, 1 if vote == 'vote-up' else -1)
+    # if vote == 'vote-up':
+    #     for item in questions:
+    #         if item['id'] == question_id:
+    #             item['vote_number'] = str(int(item['vote_number']) + 1)
+    # elif vote == 'vote-down':
+    #     for item in questions:
+    #         if item['id'] == question_id:
+    #             item['vote_number'] = str(int(item['vote_number']) + 1)
     data_handler.write_table_to_file_question(data_handler.create_list_to_write(questions))
     return redirect("/list")
 
@@ -161,17 +140,18 @@ def vote_question(question_id, vote):
 @app.route('/answer/<answer_id>/<vote>')
 def vote_answer(answer_id, vote):
     answers = data_handler.get_all_answer()
-    question_id= ''
-    if vote == 'vote-up':
-        for item in answers:
-            if item['id']==answer_id:
-                question_id = item['question_id']
-                item['vote_number']= str(int(item['vote_number'])+1)
-    elif vote == 'vote-down':
-        for item in answers:
-            if item['id']==answer_id:
-                question_id = item['question_id']
-                item['vote_number']= str(int(item['vote_number'])-1)
+    question_id = int([a['question_id'] for a in answers if a['id'] == answer_id].pop())
+    answers = additional_functions.vote(answers, answer_id, 1 if vote == 'vote-up' else -1)
+    # if vote == 'vote-up':
+    #     for item in answers:
+    #         if item['id'] == answer_id:
+    #             question_id = item['question_id']
+    #             item['vote_number'] = str(int(item['vote_number']) + 1)
+    # elif vote == 'vote-down':
+    #     for item in answers:
+    #         if item['id'] == answer_id:
+    #             question_id = item['question_id']
+    #             item['vote_number'] = str(int(item['vote_number']) - 1)
     data_handler.write_table_to_file_answer(data_handler.create_list_to_write(answers))
     link_direct = "/question/" + str(question_id)
     return redirect(link_direct)
