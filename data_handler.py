da90592e484b76b25200e088356e2b6c0ef57154
Alex Andrzejewicz <andrzejewiczalex@gmail.com>
2022-04-26 21:39:44 +0200

    Add some SQL to project

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,12 @@
 import os
-
+import datetime
 DATA_FILE_PATH_QUESTION = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
 DATA_FILE_PATH_ANSWER = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 DATA_HEADER_QUESTION = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 DATA_HEADER_ANSWER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
+import additional_functions
 
 import database_common
 
@@ -13,11 +14,36 @@ import database_common
 @database_common.connection_handler
 def get_questions(cursor):
     query = """
-            SELECT submission_time, view_number, vote_number, title, message, image
+            SELECT id, submission_time, view_number, vote_number, title, message, image
             FROM question
             ORDER BY submission_time"""
     cursor.execute(query)
     return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_question(cursor, q_id):
+    query = """
+            SELECT id, submission_time, view_number, vote_number, title, message, image
+            FROM question
+            WHERE id = %s
+            ORDER BY submission_time"""
+    cursor.execute(query, (q_id,))
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_answer(cursor, q_id):
+    query = """
+           SELECT id, submission_time, vote_number, question_id, message, image
+           FROM answer
+           WHERE question_id = %s
+           ORDER BY submission_time"""
+    cursor.execute(query, (q_id,))
+    return cursor.fetchall()
+
+
+
 
 @database_common.connection_handler
 def get_answers(cursor):
@@ -27,6 +53,21 @@ def get_answers(cursor):
            ORDER BY first_name"""
     cursor.execute(query)
     return cursor.fetchall()
+
+@database_common.connection_handler
+def add_question(cursor, request):
+    upload_file = request.files['file']
+    image_name = additional_functions.file_operation(upload_file)
+    message = request.form['message']
+    title = request.form.get('title', default="")
+    time = str(datetime.datetime.now().strftime("%d/%m/%y %H:%M"))
+    query = """
+    INSERT INTO question (submission_time, view_number, vote_number, title, message, image) 
+    VALUES (time,title,message,image_name )
+    
+    """
+
+
 
 
 def create_list_to_write(list):
