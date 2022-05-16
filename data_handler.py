78e1096dbeb5684be11f5e20e59e1c785e5f1c45
Patryk Skierkowski <salon.skierkowski@gmail.com>
2022-05-16 22:28:36 +0200

    repaired bug with string formating

--- data_handler.py
+++ data_handler.py
@@ -109,7 +109,7 @@ def update_view_number(cursor, q_id):
     SET view_number = view_number + 1
     WHERE id = %s
     """
-    cursor.execute(query, q_id)
+    cursor.execute(query, (q_id,))
 
 
 @database_common.connection_handler
